9cfb066f6d1fd6886f7a30233287a5f175c46ef9
PureCloud Jenkins <purecloud-jenkins@ininica.com>
2023-10-04 16:32:16 +0000

    13.5.0

--- build/src/purecloud-guest-chat-client/index.js
+++ build/src/purecloud-guest-chat-client/index.js
@@ -32,7 +32,7 @@ import WebChatApi from './api/WebChatApi.js';
  * </pre>
  * </p>
  * @module purecloud-guest-chat-client/index
- * @version 13.4.0
+ * @version 13.5.0
  */
 class platformClient {
 	constructor() {
